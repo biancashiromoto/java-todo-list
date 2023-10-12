2096b7252e69b6783f9ef8979b601d409aa00490
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 16:14:20 -0300

    chore: Add comments to Controller.java

--- src/main/java/br/com/bshiromoto/todolist/controller/Controller.java
+++ src/main/java/br/com/bshiromoto/todolist/controller/Controller.java
@@ -1,5 +1,4 @@
 // Componente designado para ser a primeira camada de acesso entre a requisição (usuário) e as demais camadas
-
 package br.com.bshiromoto.todolist.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // usado para construir APIs - o Spring tem vários outros tipos de controller
-@RequestMapping("/home") // estrutura a rota
+@RequestMapping("/home") // especifica que todas as requisições que começam com /home serão manipuladas por métodos dentro desta classe
 
 public class Controller {
-  @GetMapping("")
+  @GetMapping("") // mapeia um método para responder às requisições HTTP do tipo GET
+  
+  // método printMessage que retorna uma string
   public String printMessage() {
     return "hello world";
   }
