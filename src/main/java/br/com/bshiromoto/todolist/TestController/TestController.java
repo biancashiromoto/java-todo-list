6313f88713a05afa35e727b1c766430bba3241bb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 17:13:47 -0300

    chore: Rename Controller to TestController

--- src/main/java/br/com/bshiromoto/todolist/TestController/TestController.java
+++ src/main/java/br/com/bshiromoto/todolist/TestController/TestController.java
@@ -1,5 +1,5 @@
 // Componente designado para ser a primeira camada de acesso entre a requisição (usuário) e as demais camadas
-package br.com.bshiromoto.todolist.controller;
+package br.com.bshiromoto.todolist.TestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // usado para construir APIs - o Spring tem vários outros tipos de controller
 @RequestMapping("/home") // especifica que todas as requisições que começam com /home serão manipuladas por métodos dentro desta classe
 
-public class Controller {
+public class TestController {
   @GetMapping("") // mapeia um método para responder às requisições HTTP do tipo GET
   
   // método printMessage que retorna uma string
