3310dbe32e6eeca250b71f83a0be1f1ef245f34b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 16:41:22 -0300

    refactor: Use Lombok for getters/setters

--- src/main/java/br/com/bshiromoto/todolist/user/UserModel.java
+++ src/main/java/br/com/bshiromoto/todolist/user/UserModel.java
@@ -1,32 +1,12 @@
 package br.com.bshiromoto.todolist.user;
 
-// criação de uma classe
+import lombok.Data;
+
+@Data // cria automaticamente os getters e setters
+// se quisesse usar só os getters, usaria @Getter, e só os setters, @Setter
+
 public class UserModel {
   private String username;
   private String name;
   private String password;
-
-  public String getName() {
-    return name;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
 }
