cc8ce1dbc14365ee8bd5a9fe94bd282fbda0c1a7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 16:35:42 -0300

    feat: Add getters and setters to UserModel

--- src/main/java/br/com/bshiromoto/todolist/user/UserModel.java
+++ src/main/java/br/com/bshiromoto/todolist/user/UserModel.java
@@ -5,4 +5,28 @@ public class UserModel {
   private String username;
   private String name;
   private String password;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
 }
