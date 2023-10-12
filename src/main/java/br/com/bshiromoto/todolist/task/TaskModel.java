b15f0530a116d04e328cca91719915dfe3c92788
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 18:11:48 -0300

    feat: Create table_tasks

---
+++ src/main/java/br/com/bshiromoto/todolist/task/TaskModel.java
@@ -0,0 +1,33 @@
+package br.com.bshiromoto.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "table_tasks")
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private String description;
+
+  @Column(unique = true, length = 50)
+  private String title;
+
+  private LocalDateTime dueDate;
+  private String priority;
+  private String status;
+  private UUID userId;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
