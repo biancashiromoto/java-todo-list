e70cb3a6de17d7f1958501ca1e8dd5e3230ffaea
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 17:22:24 -0300

    feat: Change return for function create

--- src/main/java/br/com/bshiromoto/todolist/user/UserController.java
+++ src/main/java/br/com/bshiromoto/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 // Componente designado para ser a primeira camada de acesso entre a requisição (usuário) e as demais camadas
 package br.com.bshiromoto.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users") // especifica que todas as requisições que começam com /home serão manipuladas por métodos dentro desta classe
 
 public class UserController {
+  @Autowired // gerencia o ciclo de vida do método IUserRepository
+  private IUserRepository userRepository;
+
   @PostMapping("/")
 
   // o @RequestBody indica que o que for recebido no corpo da requisição virá na estrutura do UserModel
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getName());
+  public UserModel create(@RequestBody UserModel userModel) {
+    var createdUser = this.userRepository.save(userModel);
+    return createdUser;
   }
 }
