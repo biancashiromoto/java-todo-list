21cf8c6b2c46af442110016ae28843c038206c22
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-12 17:14:28 -0300

    feat: Create table_users

--- src/main/java/br/com/bshiromoto/todolist/user/UserModel.java
+++ src/main/java/br/com/bshiromoto/todolist/user/UserModel.java
@@ -1,12 +1,38 @@
 package br.com.bshiromoto.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data // cria automaticamente os getters e setters
-// se quisesse usar só os getters, usaria @Getter, e só os setters, @Setter
+// se quisesse usar só os getters, usaria @Getter, e só os setters, @Setter, por exemplo:
+//   public class UserModel {
+  // @Getter
+  // @Setter
+//   private String username; // neste caso, só seria permitido usar getter e setter de username
+//   private String name;
+//   private String password;
+// }
+
+@Entity(name = "table_users") // define um nome para a tabela
 
 public class UserModel {
+
+  @Id // informa ao db que id é a PK da tabela
+  @GeneratedValue(generator = "UUID") // faz a geração dos ids de forma automática
+  private UUID id;
+
   private String username;
   private String name;
   private String password;
+
+  @CreationTimestamp // quando o dado for gerado, o db salva automaticamente o timestamp
+  private LocalDateTime createdAt;
 }
