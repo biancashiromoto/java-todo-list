7dcce7b4bb6f9f846aed555dcb9b3a1bd5a6553c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-13 17:15:15 -0300

    feat: change route for TestController

--- src/main/java/br/com/bshiromoto/todolist/TestController/TestController.java
+++ src/main/java/br/com/bshiromoto/todolist/TestController/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // usado para construir APIs - o Spring tem vários outros tipos de controller
-@RequestMapping("/home") // especifica que todas as requisições que começam com /home serão manipuladas por métodos dentro desta classe
+@RequestMapping("/") // especifica que todas as requisições que começam com /home serão manipuladas por métodos dentro desta classe
 
 public class TestController {
   @GetMapping("") // mapeia um método para responder às requisições HTTP do tipo GET
