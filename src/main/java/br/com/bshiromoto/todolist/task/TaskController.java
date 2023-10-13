33ebd1d3b40608604cbc190e9e7c859053f3db78
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-13 13:20:06 -0300

    feat: Create function update

--- src/main/java/br/com/bshiromoto/todolist/task/TaskController.java
+++ src/main/java/br/com/bshiromoto/todolist/task/TaskController.java
@@ -2,13 +2,16 @@ package br.com.bshiromoto.todolist.task;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,5 +59,27 @@ public class TaskController {
   public ResponseEntity findAll() {
     var tasks = this.taskRepository.findAll();
     return ResponseEntity.status(HttpStatus.OK).body(tasks);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id) {
+    Optional<TaskModel> task = this.taskRepository.findById(id);
+
+    if(task.isPresent()) {
+      TaskModel foundTask = task.get();
+
+      foundTask.setTitle(taskModel.getTitle());
+      foundTask.setDescription(taskModel.getDescription());
+      foundTask.setDueDate(taskModel.getDueDate());
+      foundTask.setDueTime(taskModel.getDueTime());
+      foundTask.setPriority(taskModel.getPriority());
+      foundTask.setStatus(taskModel.getStatus());
+
+      TaskModel updatedTask = this.taskRepository.save(foundTask);
+
+      return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
+    } else {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task not found");
+    }
   }
 }
