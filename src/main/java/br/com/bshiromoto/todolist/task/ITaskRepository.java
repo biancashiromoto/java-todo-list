76c6c08a91849ff779007bb3c7765750b2734852
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-13 15:16:29 -0300

    refactor: Change expected return for findById

--- src/main/java/br/com/bshiromoto/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/bshiromoto/todolist/task/ITaskRepository.java
@@ -3,6 +3,7 @@ package br.com.bshiromoto.todolist.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -10,4 +11,6 @@ public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
   TaskModel findByTitle(String title);
 
   List<TaskModel> findAll();
+
+  Optional<TaskModel> findById(UUID id);
 }
