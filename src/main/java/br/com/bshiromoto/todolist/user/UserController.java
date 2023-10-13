1799878b798efcbfffebb292e5bd8e51f90c70dc
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-13 18:20:40 -0300

    refactor: Use UserService to hash password

--- src/main/java/br/com/bshiromoto/todolist/user/UserController.java
+++ src/main/java/br/com/bshiromoto/todolist/user/UserController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
-
 @RestController // usado para construir APIs - o Spring tem vários outros tipos de controller
 @RequestMapping("/users") // especifica que todas as requisições que começam com /home serão manipuladas por métodos dentro desta classe
 
@@ -19,32 +17,35 @@ public class UserController {
   @Autowired // gerencia o ciclo de vida do método IUserRepository
   private IUserRepository userRepository;
 
+  @Autowired
+  private UserService userService;
+
   @PostMapping("")
 
   // o ResponseEntity permite que tenhamos retornos diferentes na mesma requisição
   // o @RequestBody indica que o que for recebido no corpo da requisição virá na estrutura do UserModel
   public ResponseEntity create(@RequestBody UserModel userModel) {
-  
-    // usa o método findByUsername definido na interface
-    var foundUser = this.userRepository.findByUsername(userModel.getUsername());
+    try {
+      
+      // usa o método findByUsername definido na interface
+      var foundUser = this.userRepository.findByUsername(userModel.getUsername());
 
-    if(foundUser != null) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+      if(foundUser != null) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+      }
+      
+      String hashedPassword = userService.hashPassword(userModel.getPassword());
+      
+      userModel.setPassword(hashedPassword);
+
+      // salva os dados do usuário no db
+      var createdUser = this.userRepository.save(userModel);
+
+      // retorna o status 201 e o novo usuário
+      return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
-    
-    // usa o BCrypt e retorna um objeto configurado com as configurações padrão do bcrypt
-    var hashedPassword = BCrypt.withDefaults()
-      // gera um hash a partir de uma senha
-      // toCharArray()obtém a senha do objeto userModel como um array de caracteres
-      .hashToString(12, userModel.getPassword().toCharArray());
-
-    userModel.setPassword(hashedPassword);
-
-    // salva os dados do usuário no db
-    var createdUser = this.userRepository.save(userModel);
-
-    // retorna o status 201 e o novo usuário
-    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
   }
 
   @GetMapping("")
