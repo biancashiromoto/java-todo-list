592e84092ae29ab304846933546492da7f903afe
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-10-14 10:52:25 -0300

    chore: Change Priority types to lowercase

--- src/main/java/br/com/bshiromoto/todolist/task/types/Priority.java
+++ src/main/java/br/com/bshiromoto/todolist/task/types/Priority.java
@@ -2,7 +2,7 @@ package br.com.bshiromoto.todolist.task.types;
 
 // enum representa um conjunto de valores nomeados de forma significativa
 public enum Priority {
-  HIGH,
-  MEDIUM,
-  LOW
+  high,
+  medium,
+  low
 }
